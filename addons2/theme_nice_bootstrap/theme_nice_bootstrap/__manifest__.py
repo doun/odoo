ec3080ed0bf0ddc0f584aa1d437b6e4c90bf9cc6
doun <doun@163.com>
2018-10-24 20:35:34 +0800

    添加自定义modules

---
+++ addons2/theme_nice_bootstrap/theme_nice_bootstrap/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Theme Nice Bootstrap",
+    'version': '1.0',
+    'summary': 'Theme Nice Bootstrap',
+    'description': """
+Theme Nice Bootstrap.
+====================
+    """,
+    'author': "Do Incredible",
+    'website': "http://www.doincredible.com",
+    'category': 'Theme',
+    'depends': ['website_crm',
+                'theme_common',
+                'website_animate',
+                'snippet_google_map',
+                'snippet_latest_posts'],
+    'data': [
+        'views/assets.xml',
+        'views/website_template.xml',
+    ],
+    'images': ['static/description/main_image.png'],
+    'license': 'OPL-1',
+}
